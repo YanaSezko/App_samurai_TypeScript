07eb6a165b23d0c828d7425850627aaef14cae1f
Yana <33156136+YanaSezko@users.noreply.github.com>
2021-03-02 12:44:40 +0200

    Updates

---
+++ precache-manifest.6f6658bc0b337085dabebe03f391be7b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b501f5f8d8643b20343fa0759c8c9b4a",
+    "url": "/App_samurai_TypeScript/index.html"
+  },
+  {
+    "revision": "e11b95481a970d9bfe0d",
+    "url": "/App_samurai_TypeScript/static/css/3.3d00704e.chunk.css"
+  },
+  {
+    "revision": "7f9bc6073c206c506aed",
+    "url": "/App_samurai_TypeScript/static/css/4.23a88ac3.chunk.css"
+  },
+  {
+    "revision": "795e13dea705442802f3",
+    "url": "/App_samurai_TypeScript/static/css/main.b0e52d66.chunk.css"
+  },
+  {
+    "revision": "77d10c5f65c576be91c3",
+    "url": "/App_samurai_TypeScript/static/js/2.a08fb8e5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/App_samurai_TypeScript/static/js/2.a08fb8e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11b95481a970d9bfe0d",
+    "url": "/App_samurai_TypeScript/static/js/3.950dd49d.chunk.js"
+  },
+  {
+    "revision": "7f9bc6073c206c506aed",
+    "url": "/App_samurai_TypeScript/static/js/4.c0f44d13.chunk.js"
+  },
+  {
+    "revision": "795e13dea705442802f3",
+    "url": "/App_samurai_TypeScript/static/js/main.e257726c.chunk.js"
+  },
+  {
+    "revision": "3f4942656fbe8d049164",
+    "url": "/App_samurai_TypeScript/static/js/runtime-main.496669e2.js"
+  },
+  {
+    "revision": "50a503b3dc72b689f8015711a1843de5",
+    "url": "/App_samurai_TypeScript/static/media/logo.50a503b3.png"
+  },
+  {
+    "revision": "2569dfb731da25fc61254efcdb1fe3ff",
+    "url": "/App_samurai_TypeScript/static/media/preloader.2569dfb7.svg"
+  }
+]);
